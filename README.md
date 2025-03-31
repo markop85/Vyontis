90f228052463545355046a43ac40e06aa61d7cec
markop85 <mark.kopolov@gmail.com>
2025-03-31 12:12:37 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 # Vyontis
+=======
+# voyantis
+
+1. cd frontend -> "npm i && npm run serve"
+2. cd backend => "npm i && nodemon server.js"
+>>>>>>> d8aaa57 (Initial commit)
---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <QueueMessages />
+</template>
+
+<script>
+import QueueMessages from './components/QueueMessages.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    QueueMessages
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
